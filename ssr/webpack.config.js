fcab38d4af498790871f3adc9a053645073bd207
Mercuriver <mercuriver@gmail.com>
2020-12-07 00:17:59 +0900

    ci(ssr): Install webpack-node-externals & set webpakc.config

--- ssr/webpack.config.js
+++ ssr/webpack.config.js
@@ -1,30 +1,59 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
 
-module.exports = {
-    entry: './src/index.js',
+function getConfig(isServer) {
+  return {
+    entry: isServer ? { server: './src/server.js' } : { main: './src/index.js' },
     output: {
-        filename: '[name].[chunkhash].js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/dist/',
+      filename: isServer ? '[name].bundle.js' : '[name].[chunkhash].js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/dist/',
     },
+    target: isServer ? 'node' : 'web',
+    externals: isServer ? [nodeExternals()] : [],
+    node: {
+      __dirname: false,
+    },
+    optimization: isServer 
+    ? {
+      splitChunks: false,
+      minimize: false,
+    } : undefined,
     module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        configFile: path.resolve(__dirname, '.babelrc.client.js'),
-                    },
-                }
+      rules: [
+        {
+          test: /\.js$/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              configFile: path.resolve(
+                __dirname, 
+                isServer ? '.babelrc.server.js' : '.babelrc.client.js',
+              ),
             },
-        ],
+          },
+        },
+        {
+          test: /\.png$/,
+          use: {
+            loader: 'file-loader',
+            options: {
+              emitFile: isServer ? false : true,
+            },
+          },
+        },
+      ],
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './template/index.html',
-        }),
+    plugins: isServer 
+    ? [] 
+    : [
+      new HtmlWebpackPlugin({
+        template: './template/index.html',
+      }),
     ],
     mode: 'production',
-};
+  };
+}
+
+module.exports = [getConfig(false),getConfig(true)];
